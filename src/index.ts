08bbdf8c0d7c109c1bb9f5b734d6f67dc093f079
Frantz Kati <bahdcoder@gmail.com>
2020-05-23 22:25:49 +0100

    02-configure-app-environments

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import app from './server'
-import Config from './config'
+import app from '@/server'
+import Config from '@/config'
 
 app.listen(Config.port, () => {
     console.log('running on port', Config.port)
---
+++ src/tests/auth/register.spec.ts
@@ -0,0 +1,6 @@
+import test from 'ava'
+import Config from '@/config'
+
+test('can add two numbers', t => {
+    t.pass()
+})
